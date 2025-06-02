42b6eb62dbf315867399e4d499b12862def1e1e6
Jiwaru <jiwarujiwa@gmail.com>
2025-06-02 09:23:01 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,26 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login', ['filter' => 'redirect']);
+$routes->get('logout', 'AuthController::logout');
+
+$routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+});
+
+$routes->get('contact', 'ContactController::index', ['as' => 'contact', 'filter' => 'auth']);
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->get('faq', 'FaqController::index', ['filter' => 'auth']);
+$routes->get('contact', 'ContactController::index', ['filter' => 'auth']);
+$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
