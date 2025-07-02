1ff67302aed05be9ec2ae34e053aa2d8c637fce1
Jiwaru <jiwarujiwa@gmail.com>
2025-07-02 22:59:27 +0700

    add diskon

---
+++ app/Database/Migrations/2025-07-02-155420_Diskon.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Diskon extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'tanggal' => [
+                'type'       => 'DATE',
+                'null'       => true,
+            ],
+            'nominal' => [
+                'type' => 'double',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('diskon');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('diskon');
+    }
+}
